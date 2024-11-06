2b83d2673397ed9f5486537c5e28533c191a8d2f
pr0fix <bgx152@myy.haaga-helia.fi>
2024-11-06 15:30:14 +0200

    Made constants out of fetchCoffeeShops params

--- src/api/fetchCoffeeShops.js
+++ src/api/fetchCoffeeShops.js
@@ -1,6 +1,14 @@
 import axios from "axios";
 import { FOURSQUARE_BASE_URL, headers } from "./foursquareConfig";
 
+// Constants
+const QUERY = "coffee";
+const RADIUS = 2000;
+const LIMIT = 50;
+const SORT = "POPULARITY";
+// category id's from Foursquare Places API documentation for Cafes, Coffee, and Tea Houses & Coffee Shops
+const CATEGORIES = "63be6904847c3692a84b9bb6,4bf58dd8d48988d1e0931735";
+
 // Fetch coffee shops from Foursquare Places API
 const fetchCoffeeShops = async (latitude, longitude) => {
   try {
@@ -8,12 +16,11 @@ const fetchCoffeeShops = async (latitude, longitude) => {
       headers,
       params: {
         ll: `${latitude},${longitude}`,
-        query: "coffee",
-        radius: 2000,
-        limit: 50,
-        sort: "POPULARITY",
-        // category id's from Foursquare Places API documentation for Cafes, Coffee, and Tea Houses & Coffee Shops
-        categories: "63be6904847c3692a84b9bb6,4bf58dd8d48988d1e0931735",
+        query: QUERY,
+        radius: RADIUS,
+        limit: LIMIT,
+        sort: SORT,
+        categories: CATEGORIES,
       },
     });
     return response.data.results;
